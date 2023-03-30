b2abae9b75af47084f4965356ea6447315c5ec9c
Jared <jaredjrsong@gmail.com>
2023-03-31 01:43:42 +1100

    added screen and blob

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ function setup() {
   setupHome();
   setupHeader();
   setupGallery();
+  setupBlob();
   // setupIntro();
+}
+
+function setupBlob() {
+  const blob = document.getElementById("blob");
+
+  window.onpointermove = (event) => {
+    const { clientX, clientY } = event;
+
+    blob.animate(
+      {
+        left: `${clientX}px`,
+        top: `${clientY}px`,
+      },
+      { duration: 3000, fill: "forwards" }
+    );
+  };
 }
 
 function setupHeader() {
